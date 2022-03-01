edc905300d345320804821a0865e66cd4e15977c
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-01 05:54:53 +0100

    removed Apple FN mod

---
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -0,0 +1,9 @@
+/* This file was automatically generated. Do not edit or copy.
+ */
+
+#pragma once
+
+#define QMK_VERSION "0.16.0-24-ga14597-dirty"
+#define QMK_BUILDDATE "2022-03-01-05:52:49"
+#define CHIBIOS_VERSION "develop_2021_q3"
+#define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
--- keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |  FN  |   7  |   8  |   9  |   -  |   /  |EnDash|EmDash|   [  |   ]  | Del  |
+ * |   ` |KC_APFN|   7  |   8  |   9  |   -  |   /  |EnDash|EmDash|   [  |   ]  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |   .  |   4  |   5  |   6  |   +  |   *  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_GRV,  KC_APFN, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS,UC(0x2013),UC(0x2014),KC_LBRC,KC_RBRC,KC_DEL,
+  KC_GRV,  _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS,UC(0x2013),UC(0x2014),KC_LBRC,KC_RBRC,KC_DEL,
   KC_DEL,  KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_PEQL, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
