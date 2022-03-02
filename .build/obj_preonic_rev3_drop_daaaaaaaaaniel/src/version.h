0b98e4753afe84313b2fd4b28283f93c140db13c
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 00:36:06 +0100

    changed RAISE and LOWER into layer tap keys.

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-41-g05865d-dirty"
-#define QMK_BUILDDATE "2022-03-03-00:25:14"
+#define QMK_VERSION "0.16.0-42-gdc4372-dirty"
+#define QMK_BUILDDATE "2022-03-03-00:32:26"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
