9996fd71809465102c04e1b0f713734f804673dd
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-02 22:15:49 +0100

    recompiled new keymap and fixed error caused by extraneous ,

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-25-gedc905-dirty"
-#define QMK_BUILDDATE "2022-03-01-05:55:09"
+#define QMK_VERSION "0.16.0-35-g10601c-dirty"
+#define QMK_BUILDDATE "2022-03-02-22:14:45"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
