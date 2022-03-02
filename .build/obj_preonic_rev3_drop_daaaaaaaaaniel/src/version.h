a6e6f0047f3fc4d09200927fe55f586e2c0cf9b5
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-02 23:15:55 +0100

    recompiled keymap with layer taps

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-35-g10601c-dirty"
-#define QMK_BUILDDATE "2022-03-02-22:14:45"
+#define QMK_VERSION "0.16.0-37-g435752-dirty"
+#define QMK_BUILDDATE "2022-03-02-23:15:20"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
