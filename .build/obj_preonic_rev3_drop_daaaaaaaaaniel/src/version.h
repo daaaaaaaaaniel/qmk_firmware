94837612191337bd8d9be2a637d4606c527c50d1
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-02 23:21:52 +0100

    recompiled keymap with layer taps and mod taps for arrow keys

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-37-g435752-dirty"
-#define QMK_BUILDDATE "2022-03-02-23:15:20"
+#define QMK_VERSION "0.16.0-38-ga6e6f0-dirty"
+#define QMK_BUILDDATE "2022-03-02-23:21:14"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
