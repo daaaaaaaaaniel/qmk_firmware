05865d233a37a1680c123d5e14f223917a9a6011
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 00:23:19 +0100

    added brackets to top row QWERTY layout

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-38-ga6e6f0-dirty"
-#define QMK_BUILDDATE "2022-03-02-23:21:14"
+#define QMK_VERSION "0.16.0-40-gc0baf7d-dirty"
+#define QMK_BUILDDATE "2022-03-03-00:22:25"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
