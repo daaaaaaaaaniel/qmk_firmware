3bb1897cf5f3178eeee48d986a894f8e1c3b8cba
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 01:27:24 +0100

    Update keymap.c
    changing arrow keys in bottem left of keymap to [ [ / =

--- keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Opt  | Cmd  |Lower |    Space    |Raise | Cmd  | Opt  | Ctrl |      |
+ * |      | Ctrl | Opt  | Cmd  |Lower |    Space    |Raise |   [  |   ]  |   \  |   =  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   SFT_T(KC_ESC), KC_Z, KC_X, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-  _______, KC_LCTL, KC_LOPT, KC_LCMD, LT(_LOWER, KC_DEL), KC_SPC, KC_SPC, LT(_RAISE, KC_BSPC), RCMD_T(KC_LEFT), ROPT_T(KC_DOWN), RCTL_T(KC_UP), KC_RGHT
+  _______, KC_LCTL, KC_LOPT, KC_LCMD, LT(_LOWER, KC_DEL), KC_SPC, KC_SPC, LT(_RAISE, KC_BSPC), RCMD_T(KC_LBRC), ROPT_T(KC_RBRC), RCTL_T(KC_BSLS), KC_EQL
 ),
 
 /* Lower
@@ -79,20 +79,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |      |   7  |   8  |   9  |   -  |   /  |   -  |  Up  |   [  |   ]  |rShift|
+ * |   `  |      |   7  |   8  |   9  |   -  |   /  |   -  |  Up  |   [  |   ]  | Cmd  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |   .  |   4  |   5  |   6  |   +  |   *  | Left | Down |Right |   ]  |  \   |
+ * | Del  |   .  |   4  |   5  |   6  |   +  |   *  | Left | Down |Right |   ]  | Opt  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   0  |   1  |   2  |   3  |Enter |   =  |ISO # |ISO / | Pg Up| Pg Dn|      |
+ * |      |   0  |   1  |   2  |   3  |Enter |   =  |ISO # |ISO / | Pg Up| Pg Dn| Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_GRV,  _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS, KC_MINS, KC_UP,   KC_LBRC, KC_RBRC, KC_RSFT,
-  KC_DEL,  KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_BSLS,
-  _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_PEQL, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+  KC_GRV,  _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS, KC_MINS, KC_UP,   KC_LBRC, KC_RBRC, KC_RCMD,
+  KC_DEL,  KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_ROPT,
+  _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_PEQL, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_RSFT,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
