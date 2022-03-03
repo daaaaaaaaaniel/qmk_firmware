8f558614c35cc6a2b0377063c8f08ebd7f6621f6
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 03:24:23 +0100

    compiled firmware

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-47-g3bb1897c-dirty"
-#define QMK_BUILDDATE "2022-03-03-01:46:06"
+#define QMK_VERSION "0.16.0-49-g5e6176-dirty"
+#define QMK_BUILDDATE "2022-03-03-03:19:56"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
