5e61769c77715f88ad73254b7fe4e66aca64e700
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 02:01:53 +0100

    Update keymap.c
    Change rightmost column to mod-taps.

--- keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    RCTL_T(KC_MINS),
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ROPT_T(KC_BSPC),
+  KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCMD_T(KC_QUOT),
   SFT_T(KC_ESC), KC_Z, KC_X, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
   _______, KC_LCTL, KC_LOPT, KC_LCMD, LT(_LOWER, KC_DEL), SFT_T(KC_SPC), SFT_T(KC_SPC), LT(_RAISE, KC_BSPC), RCMD_T(KC_LBRC), ROPT_T(KC_RBRC), RCTL_T(KC_BSLS), KC_EQL
 ),
