dae7df99a421f12bc4f711f1b0a6029d66163ba8
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-03 01:53:45 +0100

    moved mods to right side in RAISE layer and made the spacebar a mod tap for shift

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-44-g0501168-dirty"
-#define QMK_BUILDDATE "2022-03-03-00:36:27"
+#define QMK_VERSION "0.16.0-47-g3bb1897c-dirty"
+#define QMK_BUILDDATE "2022-03-03-01:46:06"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
