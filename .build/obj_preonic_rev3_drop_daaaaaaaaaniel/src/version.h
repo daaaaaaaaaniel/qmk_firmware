d2fbaf696fa597a3b872bf350344655029237352
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-04 19:38:38 +0100

    moved opt arrow keys location on lower layer

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-52-g863036-dirty"
-#define QMK_BUILDDATE "2022-03-03-15:35:30"
+#define QMK_VERSION "0.16.0-55-gdf58b3-dirty"
+#define QMK_BUILDDATE "2022-03-04-19:37:57"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
