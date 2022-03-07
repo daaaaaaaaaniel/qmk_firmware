58e46219918531a5b03c9f366ed0c33bb06a4104
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-07 12:54:12 +0100

    MIDI notes shifted an octave lower

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-63-g3c53fd-dirty"
-#define QMK_BUILDDATE "2022-03-07-12:47:27"
+#define QMK_VERSION "0.16.0-64-g34eb8f-dirty"
+#define QMK_BUILDDATE "2022-03-07-12:53:50"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
