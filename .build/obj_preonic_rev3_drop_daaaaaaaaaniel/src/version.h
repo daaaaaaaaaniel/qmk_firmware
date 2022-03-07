c5b093659dcf16a2f49dff275c0c2331696b53e2
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-07 17:29:26 +0100

    messing with sound

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-108-gb52d83-dirty"
-#define QMK_BUILDDATE "2022-03-07-16:56:18"
+#define QMK_VERSION "0.16.0-111-gf2245e-dirty"
+#define QMK_BUILDDATE "2022-03-07-17:15:00"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
