0a108a9a14b0df905bb42363a08d82ce686614bf
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-03-07 13:13:23 +0100

    MIDI octive switching and modulation

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-64-g34eb8f-dirty"
-#define QMK_BUILDDATE "2022-03-07-12:53:50"
+#define QMK_VERSION "0.16.0-65-g58e462-dirty"
+#define QMK_BUILDDATE "2022-03-07-13:12:24"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
