c0eaf3d1b12f15ff8b93a2c77078297101a73da4
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-04-12 20:05:19 +0200

    Add backslash to keymap on lower layer

--- keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Left | Down |Right |      |   +  |   *  |   _  |   +  |   [  |   ]  | Shift|
+ * |      |      |      |      |      |      |      |      |      |      |   \  | Shift|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | MIDI |      |      |      |             |  Page Down  |WrdBks|WrdDel|      |      |
  * `-----------------------------------------------------------------------------------'
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   ALL_APP, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PPLS, KC_PAST, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_RSFT,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_RSFT,
   MIDI,    _______, _______, _______,     _______,   LT(_RAISE, KC_PGDN), WRD_BKS, WRD_DEL, _______, _______
 ),
 
