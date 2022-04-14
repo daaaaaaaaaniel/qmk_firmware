18d137b4243b353f56788c101056d74e3ca37a17
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-04-14 09:08:23 +0200

    Reorganized RAISE and LOWER layers into Nav and Sym layers

--- keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/keymap.c
@@ -131,28 +131,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS, AA_LCTL, KC_LOPT, KC_LCMD,     AA_LSPC,          AA_RSPC,      AA_RCMD, AA_ROPT, AA_RCTL, KC_RBRC
 ),
 
-/* Raise
+/* Raise (Navigation)
  * ,-----------------------------------------------------------------------------------.
  * |LockSc|Brght-|Brght+|MsnCtl|Lnchpd| Dict |DoNDst| Rwnd | Play | Ffwd | Mute | Ctrl |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |  Up  |      |      |      |WoSel←| Sel ←|  Up  | Sel →|WoSel→| Cmd  |
+ * |      |      |  MUp | Click|      |SwapCh|WoSel←| Sel ←|  Up  | Sel →|WoSel→| Cmd  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | Left | Down |Right |      |      |WrdBck| Left | Down |Right |WrdFwd| Opt  |
+ * |      | MLeft| MDown|MRight|RClick|      |WrdBck| Left | Down |Right |WrdFwd| Opt  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      | Cut  | Copy | Paste|      |      |      |      |   [  |   ]  | Shift|
+ * |  Esc |      | Cut  | Copy | Paste|      |      |      |      |      |      | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |   Page Up   |             |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_2x2u(
   KC_LOCK, KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_DICT, KC_DOND, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_RCTL,
-  _______, _______, KC_UP,   _______, _______, _______, SELWPRV, SEL_PRV, KC_UP,   SEL_NXT, SELWNXT, KC_RCMD,
-  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, WD_PREV, KC_LEFT, KC_DOWN, KC_RGHT, WD_NEXT, KC_ROPT,
-  _______, _______, KX_CUT,  KX_COPY, KX_PSTE, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_RSFT,
+  _______, KC_EXLM, KC_MS_U, KC_BTN1, KC_DLR, LCTL(KC_T), SELWPRV, SEL_PRV, KC_UP,   SEL_NXT, SELWNXT, KC_RCMD,
+  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, _______, WD_PREV, KC_LEFT, KC_DOWN, KC_RGHT, WD_NEXT, KC_ROPT,
+  KC_ESC,  _______, KX_CUT,  KX_COPY, KX_PSTE, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
   _______, KC_LOPT, _______, _______, LT(_LOWER, KC_PGUP),    _______,    KC_RCMD, KC_ROPT, KC_RCTL, _______
 ),
 
-/* Lower
+/* Lower (Symbols)
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |AppSwi|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |   |  |   \  | Shift|
+ * |  Esc |      |      |      |      |      |   {  |   }  |   [  |   ]     \  |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | MIDI |      |      |      |             |  Page Down  |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL,
   ALL_APP, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PLUS,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_BSLS, KC_RSFT,
+  KC_ESC,  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_BSLS, KC_PIPE,
   MIDI,    KC_LOPT, _______, _______,     _______,   LT(_RAISE, KC_PGDN), _______, _______, _______, _______
 ),
 
