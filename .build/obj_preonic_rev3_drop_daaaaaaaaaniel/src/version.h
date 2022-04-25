bb60abd382bdafe8b4da7c0961384953e48df45f
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-04-25 19:29:28 +0200

    refined Tab layer arrow cluster (by adding a new layer that acts like an modifier to change left/right to up/down)

--- .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
+++ .build/obj_preonic_rev3_drop_daaaaaaaaaniel/src/version.h
@@ -25,8 +25,8 @@
 
 #pragma once
 
-#define QMK_VERSION "0.16.0-399-ga105c6-dirty"
-#define QMK_BUILDDATE "2022-04-25-17:17:59"
+#define QMK_VERSION "0.16.0-420-ge109af-dirty"
+#define QMK_BUILDDATE "2022-04-25-19:28:23"
 #define CHIBIOS_VERSION "develop_2021_q3"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-dirty"
 
