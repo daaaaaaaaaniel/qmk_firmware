9ef381c48bb14eacfdee87a98f8c67619f70cccd
daaaaaaaaaniel <dashinbaum@gmail.com>
2022-06-02 20:41:58 +0200

    updating keymap. preparing for moving SYM to the Tab key

--- keyboards/preonic/keymaps/daaaaaaaaaniel/rules.mk
+++ keyboards/preonic/keymaps/daaaaaaaaaniel/rules.mk
@@ -8,19 +8,19 @@ BOOTLOADER = stm32-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE =  yes     # Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Enable mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 # CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = no         # Enable magic commands for debug and configuration
+COMMAND_ENABLE = no         # Magic commands for debug and configuration
 NKRO_ENABLE = yes           # N-Key Rollover (Must be set to 'no' to activate enable Apple FN patch)
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no       # Keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Keyboard RGB underglow
 AUDIO_ENABLE = yes          # Audio output
 WS2812_DRIVER = pwm
 CUSTOM_MATRIX = yes
 # ENCODER_ENABLE = yes
 # DIP_SWITCH_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes   # Key overrides
+# KEY_OVERRIDE_ENABLE = yes   # Key overrides
 
 # Do not enable RGB_MATRIX_ENABLE together with RGBLIGHT_ENABLE
 RGB_MATRIX_ENABLE = no
